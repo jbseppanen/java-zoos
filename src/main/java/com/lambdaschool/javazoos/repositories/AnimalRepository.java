be13177c17e8390dd9b16bca9e8f95855f5e577e
Jacob Seppanen <jbseppanen@gmail.com>
2019-02-26 10:49:33 -0800

    Add repositories.

---
+++ src/main/java/com/lambdaschool/javazoos/repositories/AnimalRepository.java
@@ -0,0 +1,7 @@
+package com.lambdaschool.javazoos.repositories;
+
+import com.lambdaschool.javazoos.models.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+}
